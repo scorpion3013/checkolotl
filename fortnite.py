d0e0ba134714b7936f50670caf36509431442362
scorpion <coc909scorp@gmail.com>
2019-06-12 07:03:40 +0200

    made the user_agent for fortnite changeable in the config

--- fortnite.py
+++ fortnite.py
@@ -5,7 +5,7 @@ from utils.values import settings, accounts, Proxies
 from utils.utilities import proxy_getter
 
 header_one = {
-    'User-Agent': "Fortnite/++Fortnite+Release-4.5-CL-4166199 Windows/6.2.9200.1.768.64bit",
+    'User-Agent': settings.fortnite.user_agent,
     'Authorization': settings.fortnite.authkey,
     'Content-Type': "application/x-www-form-urlencoded",
     'Accept': "*/*",
@@ -44,7 +44,7 @@ def stats(account_id, access_token, proxs_list):
     header_two = {
         'Authorization': f"bearer {access_token}",
         'Content-Type': "application/json",
-        'User-Agent': "Fortnite/++Fortnite+Release-4.5-CL-4166199 Windows/6.2.9200.1.768.64bit",
+        'User-Agent': settings.fortnite.user_agent,
         'Accept': "*/*",
         'Cache-Control': "no-cache",
         'Host': "fortnite-public-service-prod11.ol.epicgames.com",
